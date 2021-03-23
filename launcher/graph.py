d22b6c9da3c4ba89a33617ff4928c205a41fe829
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2021-03-23 18:14:13 +0100

    legend out of graph

--- launcher/graph.py
+++ launcher/graph.py
@@ -17,39 +17,39 @@ def make_graph(local_data, results, result_name, save_gif):
     # prepare graphs
     nb_graph = min(3, len(results))
     fig, axes = plt.subplots(figsize=(10, 10), ncols=nb_graph, sharey=True)
-    fig2, axes2 = plt.subplots()
+    fig2, axe2 = plt.subplots()
 
     for idGraph in range(nb_graph):
-        axes2.clear()
+        axe2.clear()
         axe1 = axes[idGraph]
         axe1.set_box_aspect(1)
         axe1.title.set_text(f"{str(results[idGraph][0])}km\n{str([x+1 for x in results[idGraph][1]])[1:-1]}")
         axe1.set_xlim(minX-1, maxX+1)
         axe1.set_ylim(minY-1, maxY+1)
 
-        axes2.set_box_aspect(1)
-        axes2.title.set_text(f"{str(results[idGraph][0])}km\n{str([x+1 for x in results[idGraph][1]])[1:-1]}")
-        axes2.set_xlim(minX-1, maxX+1)
-        axes2.set_ylim(minY-1, maxY+1)
+        axe2.set_box_aspect(1)
+        axe2.title.set_text(f"{str(results[idGraph][0])}km\n{str([x+1 for x in results[idGraph][1]])[1:-1]}")
+        axe2.set_xlim(minX-1, maxX+1)
+        axe2.set_ylim(minY-1, maxY+1)
 
         # print peaks
         for i in range(len(results[0][1])):
             x = local_data["peak"][i]["x"]
             y = local_data["peak"][i]["y"]
             axe1.scatter(x, y, c="black")
-            axes2.scatter(x, y, c="black")
+            axe2.scatter(x, y, c="black")
             axe1.text(x, y+0.5, local_data["peak"][i]["name"])
-            axes2.text(x, y+0.5, local_data["peak"][i]["name"])
+            axe2.text(x, y+0.5, local_data["peak"][i]["name"])
 
         # print traveler origin
         travel = local_data["traveler"][0]
-        axe1.scatter(travel["x"], travel["y"], c="blue")
-        axes2.scatter(travel["x"], travel["y"], c="blue")
+        axe1.scatter(travel["x"], travel["y"], c="red")
+        axe2.scatter(travel["x"], travel["y"], c="red")
 
         # print final peak
         dest = local_data["peak"][results[idGraph][1][-1]]
         axe1.scatter(dest["x"], dest["y"], c="green")
-        axes2.scatter(dest["x"], dest["y"], c="green")
+        axe2.scatter(dest["x"], dest["y"], c="green")
 
         # gif img
         fileNames = []
@@ -65,11 +65,11 @@ def make_graph(local_data, results, result_name, save_gif):
         arr1 = axe1.arrow(city[0], city[1], nexCity[0]-city[0], nexCity[1]-city[1],
                           head_width=0.15*nb_graph, head_length=0.35*nb_graph,
                           length_includes_head=True, color="red")
-        arr2 = axes2.arrow(city[0], city[1], nexCity[0]-city[0], nexCity[1]-city[1],
+        arr2 = axe2.arrow(city[0], city[1], nexCity[0]-city[0], nexCity[1]-city[1],
                            head_width=0.15*nb_graph, head_length=0.35*nb_graph,
                            length_includes_head=True, color="red")
-        axe1.legend([arr1,], [local_data["traveler"][0]["name"],])
-        axes2.legend([arr2,], [local_data["traveler"][0]["name"],])
+        axe1.legend([arr1,], [local_data["traveler"][0]["name"],], bbox_to_anchor=(0.5, -0.1), fontsize='small') # https://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
+        axe2.legend([arr2,], [local_data["traveler"][0]["name"],], bbox_to_anchor=(0.5, -0.1), fontsize='small')
 
         # gif img
         if(save_gif):
@@ -85,7 +85,7 @@ def make_graph(local_data, results, result_name, save_gif):
             axe1.arrow(city[0], city[1], nexCity[0]-city[0], nexCity[1]-city[1],
                       head_width=0.15*nb_graph, head_length=0.35*nb_graph,
                       length_includes_head=True, color="red")
-            axes2.arrow(city[0], city[1], nexCity[0]-city[0], nexCity[1]-city[1],
+            axe2.arrow(city[0], city[1], nexCity[0]-city[0], nexCity[1]-city[1],
                       head_width=0.15*nb_graph, head_length=0.35*nb_graph,
                       length_includes_head=True, color="red")
 
