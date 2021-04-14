f4e78739d889723ca0f67b64c0ca2171ccc9c224
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2021-04-14 11:12:17 +0200

    common part graph generation

--- launcher/graph.py
+++ launcher/graph.py
@@ -7,60 +7,27 @@ from geopip import search
 from threading import Thread, Lock
 import time
 from pygifsicle import optimize
+import pickle
+import io
 
 from defines import MAPS_FOLDER, RESULT_FOLDER, MAX_GRAPH, MAX_COUNTRIES
 
 
-def plot_path(link_vertices, background, show_names, countries_map, cities, x, y, x_o, y_o, x_d, y_d, results, idGraph, local_data, peaks, save_gif, fps, path, result_name, files, files_lock):
-    fig = plt.figure()
+def plot_path(idThread, fig, results, cities, local_travelers, travel_colors,
+              path, result_name, save_gif, fps, g_files, g_files_lock):
     axe = fig.gca()
-
-    axe.title.set_text(f"{results[2][1]:.1f}km")  # \n{str([x+1 for x in results[3][to:do][1]])[1:-1]}
-    axe.axis('off')
-
-    if background:
-        for country in countries_map:  # print background map
-            country.plot(ax=axe, color="lightgray", zorder=-1)
-
-    if link_vertices:
-        linked = [(id, peak["link"]) for id, peak in enumerate(peaks) if peak["origin"]]
-        for origin, dests in linked:
-            x1, y1 = cities[origin]
-            for dest in dests:
-                x2, y2 = cities[dest]
-                axe.plot([x1, x2], [y1, y2], zorder=0, linestyle="dashed", c="green", alpha=0.3)
-
-    axe.scatter(x_o, y_o, marker="s", c="red")  # , s=8
-    axe.scatter(x_d, y_d, marker="o", c="green")  # , s=12
-
-    if show_names:
-        coef = 0.3*(axe.get_xlim()[1]-axe.get_xlim()[0])/15
-        for i in range(len(cities)):
-            axe.text(x[i]+coef, y[i]+coef, local_data["peak"][i]["name"], fontsize='x-small', c="black")
-
-    # print travelers origins
-    for id, traveler in enumerate(results[-1]):
-        color = "blue"
-        travel = local_data["traveler"][id]
-        axe.scatter(travel["x"], travel["y"], marker="D", c=color)  # , s=8
-        if show_names:
-            axe.text(travel["x"]+coef, travel["y"]+coef, travel["name"], fontsize='x-small', c=color)
-
-    # end init part here
+    axe.title.set_text(f"{results[2][1]:.1f}km")
 
     filenames = []
     if(save_gif):
-        name = f"{idGraph}_-1.png"
+        name = f"{idThread}_-1.png"
         fig.savefig(name)
         filenames.append(name)
 
     # travel between peaks
     for idTravel, traveler in enumerate(results[-1]):
-        color = "blue"
-
         # print travel from origin to first peak
-        travel = local_data["traveler"][idTravel]
-        city = [travel["x"], travel["y"]]  # update here
+        city = [local_travelers[idTravel]["x"], local_travelers[idTravel]["y"]]
         nextCity = cities[traveler[1][0]]
 
         deltaX = nextCity[0]-city[0]
@@ -68,10 +35,10 @@ def plot_path(link_vertices, background, show_names, countries_map, cities, x, y
 
         axe.arrow(deltaX*0.05+city[0], deltaY*0.05+city[1], deltaX*0.9, deltaY*0.9,
                   length_includes_head=True, head_width=0.5, head_length=0.5,
-                  width=0.001, color=color, alpha=0.75)
+                  width=0.001, color=travel_colors, alpha=0.75)
 
         if(save_gif):
-            name = f"{idGraph}_{idTravel}.png"
+            name = f"{idThread}_{idTravel}.png"
             fig.savefig(name)
             filenames.append(name)
 
@@ -84,20 +51,20 @@ def plot_path(link_vertices, background, show_names, countries_map, cities, x, y
 
             axe.arrow(deltaX*0.05+city[0], deltaY*0.05+city[1], deltaX*0.9, deltaY*0.9,
                       length_includes_head=True, head_width=0.5, head_length=0.5,
-                      width=0.001, color=color, alpha=0.75)
+                      width=0.001, color=travel_colors, alpha=0.75)
 
             # gif img
             if(save_gif):
-                name = f"{idGraph}_{idTravel}_{idCity}.png"
+                name = f"{idThread}_{idTravel}_{idCity}.png"
                 fig.savefig(name)
                 filenames.append(name)
 
     # assemble gif
     if(save_gif):
-        gifname = path+RESULT_FOLDER+f"\\{result_name}_{idGraph}.gif"
+        gifname = path+RESULT_FOLDER+f"\\{result_name}_{idThread}.gif"
 
-        with files_lock:
-            files.append(gifname)
+        with g_files_lock:
+            g_files.append(gifname)
 
         frames = [imageio.imread(img) for img in filenames]
         for filename in filenames:
@@ -109,49 +76,93 @@ def plot_path(link_vertices, background, show_names, countries_map, cities, x, y
         imageio.mimsave(gifname, frames, fps=fps)
         optimize(gifname)
 
-    filename = path+RESULT_FOLDER+f"\\{result_name}_{idGraph}.png"
-    with files_lock:
-        files.append(filename)
-    fig.savefig(filename, dpi=500)
+    else:
+        filename = path+RESULT_FOLDER+f"\\{result_name}_{idThread}.png"
+        with g_files_lock:
+            g_files.append(filename)
+        fig.savefig(filename, dpi=500)
 
 
 def make_graph(path, local_data, compute_data, results, result_name, show_names, link_vertices, background, save_gif, fps):
     plt.switch_backend('agg')
-    plot_countries = []
-    countries_map = []
 
-    cities_origin = [(peak["x"], peak["y"], peak["name"]) for peak in local_data["peak"] if peak["origin"]]
-    cities_dest = [(peak["x"], peak["y"], peak["name"]) for peak in local_data["peak"] if not peak["origin"]]
+    # prepare graphs
+    nb_graph = min(MAX_GRAPH, len(results))
+
     cities = [(peak["x"], peak["y"]) for peak in local_data["peak"]]
-    x_o, y_o, _ = zip(*cities_origin)
-    x_d, y_d, _ = zip(*cities_dest)
     x, y = zip(*cities)  # extract couples [x, y]
 
+    # plot common part
+    fig = plt.figure()
+    axe = fig.gca()
+    axe.axis('off')
+
+    # step1 : optional background (country maps)
     if background:
+        plot_countries = []
         # get countries to plot
         for city in cities:
             res = search(city[0], city[1])
             if res and res["ISO3"] not in plot_countries:
                 plot_countries.append(res["ISO3"])
+
         if len(plot_countries) > MAX_COUNTRIES or len(plot_countries) == 0:
             plot_countries = ["WORLD"]
 
-        for country in plot_countries:
-            countries_map.append(geopandas.read_file(path+MAPS_FOLDER+"\\"+country+".shp"))
+        for country in plot_countries:  # print background map
+            country_map = geopandas.read_file(path+MAPS_FOLDER+"\\"+country+".shp")
+            country_map.plot(ax=axe, color="lightgray", zorder=-1)
 
-    # prepare graphs
-    nb_graph = min(MAX_GRAPH, len(results))
+    # step2 : optional display of link between vertices
+    if link_vertices:
+        linked = [(id, peak["link"]) for id, peak in enumerate(compute_data["peak"]) if peak["origin"]]
+        for origin, dests in linked:
+            x1, y1 = cities[origin]
+            for dest in dests:
+                x2, y2 = cities[dest]
+                axe.plot([x1, x2], [y1, y2], zorder=0, linestyle="dashed", c="green", alpha=0.3)
 
-    files = []
-    files_lock = Lock()
+    # step3 : display origins, destinations and travelers origins
+    cities_origin = [(peak["x"], peak["y"]) for peak in local_data["peak"] if peak["origin"]]
+    cities_dest = [(peak["x"], peak["y"]) for peak in local_data["peak"] if not peak["origin"]]
+    x_o, y_o = zip(*cities_origin) # donne direct ?
+    x_d, y_d = zip(*cities_dest)
 
+    axe.scatter(x_o, y_o, marker="s", c="red")  # , s=8
+    axe.scatter(x_d, y_d, marker="o", c="green")  # , s=12
+
+    travelers = [(trav["x"], trav["y"], trav["name"]) for trav in local_data["traveler"]]
+    x_trav, y_trav, _ = zip(*travelers)
+    axe.scatter(x_trav, y_trav, marker="D", c="blue")  # color array
+
+    # step4 : optional display of vertices and travelers names
+    if show_names:
+        coef = 0.3*(axe.get_xlim()[1]-axe.get_xlim()[0])/15
+        for i in range(len(cities)):
+            axe.text(x[i]+coef, y[i]+coef, local_data["peak"][i]["name"],
+                     fontsize='x-small', c="black")
+
+        for x, y, name in travelers:
+            axe.text(x+coef, y+coef, name, fontsize='x-small', c="blue")  # color array
+
+    # step5 : dump base graph and enrich it in each thread
+    graph_buffer = io.BytesIO()
+    pickle.dump(fig, graph_buffer)
+
+    # step6 : start threads
+    g_files = []
+    g_files_lock = Lock()
     threads = []
-    for idGraph in range(nb_graph):
-        t = Thread(target=plot_path, args=(link_vertices, background, show_names, countries_map, cities, x, y, x_o, y_o, x_d, y_d, results[idGraph], idGraph, local_data, compute_data["peak"], save_gif, fps, path, result_name, files, files_lock))
-        threads.append(t)
-        t.start()
+    for idThread in range(nb_graph):
+        graph_buffer.seek(0)  # crucial
+        newfig = pickle.load(graph_buffer)
+        args = (idThread, newfig, results[idThread], cities,
+                local_data["traveler"], "blue",  # color array
+                path, result_name, save_gif, fps, g_files, g_files_lock)
+        threads.append(Thread(target=plot_path, args=args))
 
     for thread in threads:
+        thread.start()
         thread.join()
 
-    return files
+    return g_files
--- launcher/main.py
+++ launcher/main.py
@@ -1,6 +1,5 @@
 import pathlib
 import asyncio
-import traceback
 import os
 import json
 from datetime import datetime
@@ -15,79 +14,75 @@ from graph import make_graph
 from defines import RESULT_FOLDER, DASHBOAD_URL
 
 if __name__ == "__main__":
-    try:
-        print(f"{datetime.now().time()} - Initializing...\n")
-        compile_loader()
-        path = str(pathlib.Path(__file__).parent.absolute())
+    print(f"{datetime.now().time()} - Initializing...\n")
+    compile_loader()
+    path = str(pathlib.Path(__file__).parent.absolute())
 
-        if not os.path.exists(path+RESULT_FOLDER):
-            os.makedirs(path+RESULT_FOLDER)
+    if not os.path.exists(path+RESULT_FOLDER):
+        os.makedirs(path+RESULT_FOLDER)
 
-        print(f"{datetime.now().time()} - Retrieve inputs...\n")
-        config_name, online = user_args(path)
+    print(f"{datetime.now().time()} - Retrieve inputs...\n")
+    config_name, online = user_args(path)
 
-        if online:
-            drive = Synchronize(path)
-            config = drive.set_configfile(config_name).get_config()
-        else:
-            config = load_file(config_name)
+    if online:
+        drive = Synchronize(path)
+        config = drive.set_configfile(config_name).get_config()
+    else:
+        config = load_file(config_name)
 
-        config = config_verif(path, json.loads(config))
+    config = config_verif(path, json.loads(config))
 
-        if online:
-            datafile = drive.set_datafile(config["input_datafile"]).get_input()
-        else:
-            if config["input_datafile"][-5:] != ".data":
-                config["input_datafile"] += ".data"
-            datafile = load_file(path+"\\..\\"+config["input_datafile"])
+    if online:
+        datafile = drive.set_datafile(config["input_datafile"]).get_input()
+    else:
+        if config["input_datafile"][-5:] != ".data":
+            config["input_datafile"] += ".data"
+        datafile = load_file(path+"\\..\\"+config["input_datafile"])
 
-        print(f"{datetime.now().time()} - Parsing input...\n")
-        local_data, to_compute = load_data(datafile)
+    print(f"{datetime.now().time()} - Parsing input...\n")
+    local_data, to_compute = load_data(datafile)
 
-        print(f"{datetime.now().time()} - Simulate paths...\n")
-        inputs = (config["path_generation"]["batch_size"],
-                  config["path_generation"]["nb_process"],
-                  config["path_generation"]["algorithm"])
-        results = asyncio.run(execute_heuristic(to_compute, *inputs))
+    print(f"{datetime.now().time()} - Simulate paths...\n")
+    inputs = (config["path_generation"]["batch_size"],
+                config["path_generation"]["nb_process"],
+                config["path_generation"]["algorithm"])
+    results = asyncio.run(execute_heuristic(to_compute, *inputs))
 
-        if config["results"]["print_console"]:
-            print(f"{datetime.now().time()} - Display results...\n")
-            print_results(local_data, results)
+    if config["results"]["print_console"]:
+        print(f"{datetime.now().time()} - Display results...\n")
+        print_results(local_data, results)
 
-        print(f"{datetime.now().time()} - Prepare CSV...\n")
-        path_csv, cities_csv = format_csv(local_data, results)
-        if config["results"]["keep_local"]:
-            result_path = path+RESULT_FOLDER+"\\"+config['input_datafile']+"_{0}.csv"
-            save_csv(result_path.format("paths"), path_csv)
-            save_csv(result_path.format("cities"), cities_csv)
+    print(f"{datetime.now().time()} - Prepare CSV...\n")
+    path_csv, cities_csv = format_csv(local_data, results)
+    if config["results"]["keep_local"]:
+        result_path = path+RESULT_FOLDER+"\\"+config['input_datafile']+"_{0}.csv"
+        save_csv(result_path.format("paths"), path_csv)
+        save_csv(result_path.format("cities"), cities_csv)
 
+    if config["results"]["graph"]["make"]:
+        print(f"{datetime.now().time()} - Generate graphs...\n")
+        inputs = (config["input_datafile"],
+                    config["results"]["graph"]["show_names"],
+                    config["results"]["graph"]["link_vertices"],
+                    config["results"]["graph"]["map_background"],
+                    config["results"]["graph"]["gif_mode"],
+                    config["results"]["graph"]["fps"])
+        files = make_graph(path, local_data, to_compute, results, *inputs)
+
+    if online:
+        print(f"{datetime.now().time()} - Upload data...\n")
         if config["results"]["graph"]["make"]:
-            print(f"{datetime.now().time()} - Generate graphs...\n")
-            inputs = (config["input_datafile"],
-                      config["results"]["graph"]["show_names"],
-                      config["results"]["graph"]["link_vertices"],
-                      config["results"]["graph"]["map_background"],
-                      config["results"]["graph"]["gif_mode"],
-                      config["results"]["graph"]["fps"])
-            files = make_graph(path, local_data, to_compute, results, *inputs)
+            drive.upload_imgs(config["results"]["graph"]["gif_mode"])
+        else:
+            path_csv[1].append("--")
+            drive.upload_csv(path_csv, cities_csv)
 
-        if online:
-            print(f"{datetime.now().time()} - Upload data...\n")
-            if config["results"]["graph"]["make"]:
-                drive.upload_imgs(config["results"]["graph"]["gif_mode"])
-            else:
-                path_csv[1].append("--")
-                drive.upload_csv(path_csv, cities_csv)
+    if config["results"]["graph"]["make"] and \
+            not config["results"]["keep_local"]:
+        print(f"{datetime.now().time()} - Clear directory...\n")
+        [os.remove(file) for file in files if os.path.exists(file)]
 
-        if config["results"]["graph"]["make"] and \
-                not config["results"]["keep_local"]:
-            print(f"{datetime.now().time()} - Clear directory...\n")
-            [os.remove(file) for file in files if os.path.exists(file)]
+    print(f"{datetime.now().time()} - Everything done!\n")
 
-        print(f"{datetime.now().time()} - Everything done!\n")
-
-        if online:
-            print(f"Please consult results on dashboard :\n{DASHBOAD_URL}\n")
-
-    except Exception as e:
-        traceback.print_exc(e)
+    if online:
+        print(f"Please consult results on dashboard :\n{DASHBOAD_URL}\n")
