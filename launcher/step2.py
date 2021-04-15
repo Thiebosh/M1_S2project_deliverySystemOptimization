d3e036572f798ad5a52b3f91bafa956d6c713354
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2021-04-15 10:59:03 +0200

    bug fix / simplification

--- launcher/step2.py
+++ launcher/step2.py
@@ -2,7 +2,7 @@ from defines import TMP_FILE, TMP2_FILE
 from subprocess import run
 
 
-def path_fusion(distance_matrix, results, exe_path):
+def path_fusion(distances, results, exe_path):
     file_path = exe_path[:exe_path.rfind("\\")]+TMP_FILE
     file2_path = exe_path[:exe_path.rfind("\\")]+TMP2_FILE
 
@@ -13,7 +13,7 @@ def path_fusion(distance_matrix, results, exe_path):
 
         if len(smooth_paths[-1]) != 1:  # plus d'un chemin : doit les assembler
             smooth_paths[-1] = assemble_travs(exe_path, file_path, file2_path,
-                                             distance_matrix, smooth_paths[-1])
+                                              distances, smooth_paths[-1])
 
     data = "\n".join([str(path)[1:-1] for path in smooth_paths])
     open(file2_path, "w").write(data)
@@ -24,24 +24,20 @@ def path_fusion(distance_matrix, results, exe_path):
     return ""  # fusionned
 
 
-def assemble_travs(exe_path, file_path, file2_path, distance_matrix, travs_paths):
+def assemble_travs(exe_path, file_path, file2_path, distances, travs_paths):
     size = len(travs_paths)
-    matrix = [[[0]*3 for _ in range(size)] for _ in range(size)]
-    
-    for id_A in range(size):
-        beginA = travs_paths[id_A][0]
-        endA = travs_paths[id_A][-1]
-
-        for id_B in range(size):
-            if id_A == id_B:
-                continue
-
-            beginB = travs_paths[id_B][0]
-            endB = travs_paths[id_B][-1]
-
-            matrix[id_A][id_B][0] = distance_matrix[beginA][beginB]
-            matrix[id_A][id_B][1] = distance_matrix[endB][beginA]
-            matrix[id_A][id_B][2] = distance_matrix[beginB][endB]
+    matrix = [
+                [
+                    [
+                        distances[travs_paths[id_A][-1]][travs_paths[id_B][0]],
+                        distances[travs_paths[id_A][-1]][travs_paths[id_B][-1]]
+                    ]
+                    if id_A != id_B
+                    else [0]*2
+                    for id_B in range(size)
+                ]
+                for id_A in range(size)
+             ]
 
     open(file2_path, "w").write(str(matrix))
     # optimal = run([exe_path, file_path, file2_path], capture_output=True, text=True)
@@ -58,5 +54,5 @@ def assemble_travs(exe_path, file_path, file2_path, distance_matrix, travs_paths
             travs_paths[id].reverse()
 
         assembled += travs_paths[id]
-    
+
     return assembled
